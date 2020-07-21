9401dbbb1589eb49d529a146efc094d74c53d418
adamgerhartz <adamgerhartz93@byui.edu>
2020-07-21 05:52:05 -0600

    Add TODO

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,5 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Toast.makeText(this, "Hello from Android!", Toast.LENGTH_LONG).show()
+
+        // TODO do something
     }
 }
